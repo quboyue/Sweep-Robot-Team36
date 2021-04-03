e13dcda305aabd8629a157fc3c64de6ec47d0d9d
quboyue <43964140+quboyue@users.noreply.github.com>
2021-04-03 23:10:40 +0800

    Compile file FOR Raspberry Pi 3b
    Compiled on Raspberry Pi 3b

---
+++ LIDAR_rpi/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/quboyue/LIDAR_rpi/main.cpp" "/home/ubuntu/quboyue/LIDAR_rpi/CMakeFiles/main.dir/main.cpp.o"
+  "/home/ubuntu/quboyue/LIDAR_rpi/net_serial.cpp" "/home/ubuntu/quboyue/LIDAR_rpi/CMakeFiles/main.dir/net_serial.cpp.o"
+  "/home/ubuntu/quboyue/LIDAR_rpi/net_socket.cpp" "/home/ubuntu/quboyue/LIDAR_rpi/CMakeFiles/main.dir/net_socket.cpp.o"
+  "/home/ubuntu/quboyue/LIDAR_rpi/rplidar_driver.cpp" "/home/ubuntu/quboyue/LIDAR_rpi/CMakeFiles/main.dir/rplidar_driver.cpp.o"
+  "/home/ubuntu/quboyue/LIDAR_rpi/thread.cpp" "/home/ubuntu/quboyue/LIDAR_rpi/CMakeFiles/main.dir/thread.cpp.o"
+  "/home/ubuntu/quboyue/LIDAR_rpi/timer.cpp" "/home/ubuntu/quboyue/LIDAR_rpi/CMakeFiles/main.dir/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LIDAR_rpi/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/net_serial.cpp.o"
+  "CMakeFiles/main.dir/net_socket.cpp.o"
+  "CMakeFiles/main.dir/rplidar_driver.cpp.o"
+  "CMakeFiles/main.dir/thread.cpp.o"
+  "CMakeFiles/main.dir/timer.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
