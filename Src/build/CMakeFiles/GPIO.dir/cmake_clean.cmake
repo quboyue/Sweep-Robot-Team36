ac5e1a323b892c71849fcb0bfb5b75659ed312a6
quboyue <43964140+quboyue@users.noreply.github.com>
2021-04-18 22:12:45 -0500

    Note：don't delete png in build

---
+++ Src/build/CMakeFiles/GPIO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPIO.dir/GPIO/GPIO.cpp.o"
+  "GPIO.pdb"
+  "GPIO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GPIO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
