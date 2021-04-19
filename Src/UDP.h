95550c72cb8d64478b4d2e69e1f2bf9752dadfd7
quboyue <43964140+quboyue@users.noreply.github.com>
2021-04-18 22:42:37 -0500

    Make sure files are exactly the same as rasp

--- Src/UDP.h
+++ Src/UDP.h
@@ -1,6 +1,8 @@
 #pragma once
-#include <winsock2.h>
-#include <WS2tcpip.h>
+//#include <winsock2.h>
+//#include <WS2tcpip.h>
+#include <netinet/in.h>
+#include <unistd.h>
 #include <iostream>
 #include <string>
 using namespace  std;
